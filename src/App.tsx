199d822d0cd820b366d7e9091263b772c7231b51
kalakioski <71070273+kalakioski@users.noreply.github.com>
2022-05-07 19:53:23 +0300

    Added styling packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
+import { createTheme } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
 
 import Routes from './Routes';
 
+// create material UI theme
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#f2aa26',
+			dark: '#f09c01',
+		},
+		secondary: {
+			main: '#11cb5f',
+		},
+		text: {
+			primary: '#373585',
+			secondary: '#a4a6b3',
+		},
+		background: {
+			default: '#f7f8fc',
+		},
+	},
+	typography: {
+		fontFamily: 'Poppins, sans- serif',
+		fontWeightBold: 700,
+		fontWeightMedium: 600,
+		fontWeightRegular: 400,
+		htmlFontSize: 8,
+	},
+});
+
 function App() {
 	return (
-		<div className="App">
-			<Routes />
-		</div>
+		<ThemeProvider theme={theme}>
+			<div className="App">
+				<Routes />
+			</div>
+		</ThemeProvider>
 	);
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+
+import './home.scss';
 
 const Home = () => {
-	return <div>Home Page</div>;
+	return (
+		<div className="home">
+			Home Page
+			<Button color="primary">Hello</Button>
+		</div>
+	);
 };
 
 export default Home;
